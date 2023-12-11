5ee6804dc8b23b24a4462a59b07820c4f10afd41
grey bird <hitec16@naver.com>
2023-12-12 06:04:11 +0900

    comment edited

--- app.py
+++ app.py
@@ -9,18 +9,13 @@ app = Flask(__name__)
 app.secret_key = os.urandom(16)
 app.config['MAX_CONTENT_LENGTH'] = 80 * 1024 * 1024
 
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
 		inp = request.form.get("keyword")
 		return render_template_string(inp)
 	return render_template("home.html")
-
-
-# @app.route("/result")
-# def solu():
-# 	data = request.form.get("keyword")
-# 	return data
 
 
 if __name__ == "__main__":
