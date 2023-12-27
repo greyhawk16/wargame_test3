aeaf0eb8a47e9c7457decce2331351318c873516
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2023-12-28 07:24:07 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,103 @@
-# creates EC2 instance
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
+
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
+
+# role 생성
+resource "aws_iam_role" "test_role" {
+  name               = "joonhun_SSTI_STS_role-TF7"
+  path               = "/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+
+# STS 관련 policy 생성
+resource "aws_iam_role_policy" "hello_sts" {
+  name   = "joonhun_SSTI_STS-TF7"
+  role   = aws_iam_role.test_role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "sts:GetSessionToken",
+                "sts:DecodeAuthorizationMessage",
+                "sts:GetAccessKeyInfo",
+                "sts:GetCallerIdentity",
+                "sts:AssumeRole",
+                "sts:GetServiceBearerToken"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "sts:*",
+            "Resource": "arn:aws:iam::405607210241:role/joonhun_SSTI_role"
+        }
+    ]
+}
+EOF
+}
+
+# iamReadOnly 정책 생성
+resource "aws_iam_role_policy" "iamReadOnly" {
+  name   = "joonhun_IAMReadOnly-TF7"
+  role   = aws_iam_role.test_role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:GenerateCredentialReport",
+                "iam:GenerateServiceLastAccessedDetails",
+                "iam:Get*",
+                "iam:List*",
+                "iam:SimulateCustomPolicy",
+                "iam:SimulatePrincipalPolicy"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+# EC2 인스턴스 프로필 생성
+resource "aws_iam_instance_profile" "joonhun_EC2_profile-TF7" {
+  name = "joonhun_EC2_profile-TF7"
+  role = "${aws_iam_role.test_role.name}"
+}
+
 
 
 resource "tls_private_key" "this" {
@@ -22,12 +106,12 @@ resource "tls_private_key" "this" {
 }
 
 resource "aws_key_pair" "this" {
-  key_name      = "joonhun_SSTI_key_TF5"
+  key_name      = "joonhun_SSTI_key_TF7"
   public_key    = tls_private_key.this.public_key_openssh
 
   provisioner "local-exec" {
     command = <<-EOT
-      echo "${tls_private_key.this.private_key_pem}" > joonhun_SSTI_key_TF5.pem
+      echo "${tls_private_key.this.private_key_pem}" > joonhun_SSTI_key_TF7.pem
     EOT
   }
 }
@@ -62,9 +146,10 @@ resource "aws_instance" "app_server" {
   instance_type = "t3.micro"
   key_name = "${aws_key_pair.this.key_name}"
   vpc_security_group_ids = ["${aws_security_group.alone_web.id}"] # ["sg-0f1333e985b024d83"]
+  iam_instance_profile = "${aws_iam_instance_profile.joonhun_EC2_profile-TF7.name}"
   
   tags = {
-    Name = "joonhun_ExampleAppServerInstance-TF5"
+    Name = "joonhun_ExampleAppServerInstance-TF7"
   }
   root_block_device {
     volume_size         = 30 
