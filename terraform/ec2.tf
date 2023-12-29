7f150c359305bf10d22e1373dc1ce749e9b36364
greyhawk16 <hitec16@naver.com>
2023-12-29 14:26:56 +0900

    Removed Unnecessary inline-policies

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -19,61 +19,11 @@ resource "aws_iam_role" "test_role" {
   EOF
 }
 
-# STS 관련 policy 생성
-resource "aws_iam_role_policy" "hello_sts" {
-  name   = "joonhun_SSTI_STS-TF8"
-  role   = aws_iam_role.test_role.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "sts:GetSessionToken",
-                "sts:DecodeAuthorizationMessage",
-                "sts:GetAccessKeyInfo",
-                "sts:GetCallerIdentity",
-                "sts:AssumeRole",
-                "sts:GetServiceBearerToken"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Sid": "VisualEditor1",
-            "Effect": "Allow",
-            "Action": "sts:*",
-            "Resource": "arn:aws:iam::405607210241:role/joonhun_SSTI_role"
-        }
-    ]
-}
-EOF
-}
 
 # iamReadOnly 정책 생성
-resource "aws_iam_role_policy" "iamReadOnly" {
-  name   = "joonhun_IAMReadOnly-TF8"
-  role   = aws_iam_role.test_role.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:GenerateCredentialReport",
-                "iam:GenerateServiceLastAccessedDetails",
-                "iam:Get*",
-                "iam:List*",
-                "iam:SimulateCustomPolicy",
-                "iam:SimulatePrincipalPolicy"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
+resource "aws_iam_role_policy_attachment" "iamReadOnly" {
+  role   = aws_iam_role.test_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
 }
 
 
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,68 +20,7 @@ resource "aws_iam_role" "secret_role" {
 }
 
 # SSTI_secret_role에 붙일 policy
-resource "aws_iam_role_policy" "secretsmanager_policy" {
-  name   = "joonhun_SSTI_secretsmanager_policy-TF8"
-  role   = aws_iam_role.secret_role.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "secretsmanager:*",
-                "cloudformation:CreateChangeSet",
-                "cloudformation:DescribeChangeSet",
-                "cloudformation:DescribeStackResource",
-                "cloudformation:DescribeStacks",
-                "cloudformation:ExecuteChangeSet",
-                "docdb-elastic:GetCluster",
-                "docdb-elastic:ListClusters",
-                "ec2:DescribeSecurityGroups",
-                "ec2:DescribeSubnets",
-                "ec2:DescribeVpcs",
-                "kms:DescribeKey",
-                "kms:ListAliases",
-                "kms:ListKeys",
-                "lambda:ListFunctions",
-                "rds:DescribeDBClusters",
-                "rds:DescribeDBInstances",
-                "redshift:DescribeClusters",
-                "tag:GetResources"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "lambda:AddPermission",
-                "lambda:CreateFunction",
-                "lambda:GetFunction",
-                "lambda:InvokeFunction",
-                "lambda:UpdateFunctionConfiguration"
-            ],
-            "Resource": "arn:aws:lambda:*:*:function:SecretsManager*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "serverlessrepo:CreateCloudFormationChangeSet",
-                "serverlessrepo:GetApplication"
-            ],
-            "Resource": "arn:aws:serverlessrepo:*:*:applications/SecretsManager*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::awsserverlessrepo-changesets*",
-                "arn:aws:s3:::secrets-manager-rotation-apps-*/*"
-            ]
-        }
-    ]
-}
-EOF
+resource "aws_iam_role_policy_attachment" "secretsmanager_policy" {
+  role   = aws_iam_role.secret_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
